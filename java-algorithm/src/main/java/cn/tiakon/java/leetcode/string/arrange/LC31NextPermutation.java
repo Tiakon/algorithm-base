fd792053173d3dbde70577f238cf3c65d49e0bb5
kai.tian <kai.tian@ngaa.com.cn>
2023-08-05 11:41:39 +0800

    Added the code for arrange.

---
+++ java-algorithm/src/main/java/cn/tiakon/java/leetcode/string/arrange/LC31NextPermutation.java
@@ -0,0 +1,12 @@
+package cn.tiakon.java.leetcode.string.arrange;
+
+/**
+ * 31. 下一个排列
+ * https://leetcode.cn/problems/next-permutation/
+ * @author tiankai.me@gmail.com on 2022/11/21 下午3:07.
+ */
+public class LC31NextPermutation {
+    public void nextPermutation(int[] nums) {
+
+    }
+}
---
+++ java-algorithm/src/main/java/cn/tiakon/java/leetcode/string/arrange/LC556NextGreaterElementIII.java
@@ -0,0 +1,9 @@
+package cn.tiakon.java.leetcode.stack;
+
+/**
+ * 556. 下一个更大元素 III
+ * https://leetcode.cn/problems/next-greater-element-iii/
+ * @author tiankai.me@gmail.com on 2022/11/21 上午11:51.
+ */
+public class LC556NextGreaterElementIII {
+}
