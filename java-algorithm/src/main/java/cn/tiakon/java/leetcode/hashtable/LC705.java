518f370b8cc31489e6ce4edaffe697415a538152
kai.tian <kai.tian@ngaa.com.cn>
2023-08-09 19:10:55 +0800

    Update the code for LC705、Lc706.

---
+++ java-algorithm/src/main/java/cn/tiakon/java/leetcode/hashtable/LC705.java
@@ -0,0 +1,92 @@
+package cn.tiakon.java.leetcode.hashtable;
+
+/**
+ * 705. 设计哈希集合
+ * https://leetcode.cn/problems/design-hashset/description/
+ *
+ * @author tiankai.me@gmail.com on 2023-08-09 7:48.
+ */
+public class LC705 {
+}
+
+class MyHashSet {
+    int[] bs = new int[40000];
+    public void add(int key) {
+        int bucketIdx = key / 32;
+        int bitIdx = key % 32;
+        setVal(bucketIdx, bitIdx, true);
+    }
+
+    public void remove(int key) {
+        int bucketIdx = key / 32;
+        int bitIdx = key % 32;
+        setVal(bucketIdx, bitIdx, false);
+    }
+
+    public boolean contains(int key) {
+        int bucketIdx = key / 32;
+        int bitIdx = key % 32;
+        return getVal(bucketIdx, bitIdx);
+    }
+
+    void setVal(int bucket, int loc, boolean val) {
+        if (val) {
+            int u = bs[bucket] | (1 << loc);
+            bs[bucket] = u;
+        } else {
+            int u = bs[bucket] & ~(1 << loc);
+            bs[bucket] = u;
+        }
+    }
+
+    boolean getVal(int bucket, int loc) {
+        int u = (bs[bucket] >> loc) & 1;
+        return u == 1;
+    }
+}
+
+/**
+ * 复杂度分析
+ * 时间复杂度：O(1)
+ * 空间复杂度：O(1)
+ * @author tiankai.me@gmail.com on 2023-08-09 16:27.
+ */
+class MyHashSetBooleanArray {
+    boolean[] map;
+
+    public MyHashSetBooleanArray() {
+        map = new boolean[(int) Math.pow(10, 6) + 1];
+    }
+
+    public void add(int key) {
+        map[key] = true;
+    }
+
+    public void remove(int key) {
+        map[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return map[key];
+    }
+}
+
+class MyHashSetMap {
+    MyHashMap map;
+
+    public MyHashSetMap() {
+        map = new MyHashMap();
+    }
+
+    public void add(int key) {
+        map.put(key, 0);
+    }
+
+    public void remove(int key) {
+        map.remove(key);
+    }
+
+    public boolean contains(int key) {
+        return map.get(key) != -1;
+    }
+}
---
+++ java-algorithm/src/main/java/cn/tiakon/java/leetcode/hashtable/LC706.java
@@ -0,0 +1,482 @@
+package cn.tiakon.java.leetcode.hashtable;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 706. 设计哈希映射
+ * https://leetcode.cn/problems/design-hashmap/description/
+ *
+ * @author tiankai.me@gmail.com on 2023-08-09 7:48.
+ */
+public class LC706 {
+
+    @Test
+    public void case01Test() {
+        final MyHashMap myHashMap = new MyHashMap();
+        myHashMap.put(1, 1);
+        Assert.assertEquals(1, myHashMap.get(1));
+        myHashMap.put(2, 2);
+        Assert.assertEquals(2, myHashMap.get(2));
+        Assert.assertEquals(-1, myHashMap.get(3));
+        myHashMap.put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+        Assert.assertEquals(2, myHashMap.size);
+        Assert.assertEquals(1, myHashMap.get(2));
+        myHashMap.remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+        Assert.assertEquals(-1, myHashMap.get(2));
+        Assert.assertEquals(1, myHashMap.size);
+    }
+
+    @Test
+    public void case02Test() {
+        final MyHashMap myHashMap = new MyHashMap();
+        myHashMap.put(13, 1);
+        myHashMap.put(26, 1);
+        myHashMap.put(39, 1);
+        myHashMap.put(14, 1);
+        myHashMap.put(28, 1);
+        myHashMap.put(42, 1);
+        myHashMap.put(15, 1);
+        myHashMap.put(30, 1);
+        myHashMap.put(45, 1);
+        myHashMap.put(16, 1);
+        myHashMap.put(32, 1);
+        myHashMap.put(48, 1);
+        Assert.assertEquals(1, myHashMap.get(45));
+        Assert.assertEquals(12, myHashMap.size);
+
+        myHashMap.remove(28);
+        myHashMap.remove(14);
+        myHashMap.remove(16);
+        Assert.assertEquals(9, myHashMap.size);
+        Assert.assertEquals(-1, myHashMap.get(28));
+        Assert.assertEquals(-1, myHashMap.get(14));
+        Assert.assertEquals(-1, myHashMap.get(16));
+        myHashMap.put(60, 1);
+        Assert.assertEquals(1, myHashMap.get(42));
+        Assert.assertEquals(1, myHashMap.get(15));
+        Assert.assertEquals(1, myHashMap.get(48));
+
+    }
+
+    @Test
+    public void case03Test() {
+        final MyHashMap myHashMap = new MyHashMap();
+        myHashMap.remove(27);
+        myHashMap.put(65, 65);
+        myHashMap.remove(19);
+        myHashMap.remove(0);
+        Assert.assertEquals(-1, myHashMap.get(18));
+        myHashMap.put(42, 0);
+        Assert.assertEquals(-1, myHashMap.get(19));
+        myHashMap.remove(42);
+        Assert.assertEquals(1, myHashMap.size);
+        myHashMap.put(17, 90);
+        myHashMap.put(31, 76);
+        myHashMap.put(48, 71);
+        myHashMap.put(5, 50);
+        myHashMap.put(7, 68);
+        Assert.assertEquals(6, myHashMap.size);
+        myHashMap.put(73, 74);
+        myHashMap.put(85, 18);
+        myHashMap.put(74, 95);
+        Assert.assertEquals(9, myHashMap.size);
+        myHashMap.put(84, 82);
+        myHashMap.put(59, 29);
+        myHashMap.put(71, 71);
+        Assert.assertEquals(12, myHashMap.size);
+        myHashMap.remove(42);
+        myHashMap.put(51, 40);
+        myHashMap.put(33, 76);
+        Assert.assertEquals(14, myHashMap.size);
+        Assert.assertEquals(90, myHashMap.get(17));
+        Assert.assertEquals(-1, myHashMap.get(42));
+    }
+}
+
+/**
+ * 数组+链表+自动扩容机制
+ *
+ * @author tiankai.me@gmail.com on 2023-08-09 15:12.
+ */
+class MyHashMap {
+    final float loadFactor = 0.75f;
+    int capacity = 16;
+    int size = 0;
+    Object[] arrays;
+
+    public MyHashMap() {
+        arrays = new Object[capacity];
+    }
+
+    public void put(int key, int value) {
+        resize(size + 1);
+        final int keyIndex = getIndex(key);
+        if (arrays[keyIndex] != null) {
+            Node node = (Node) arrays[keyIndex];
+            while (node != null) {
+                if (node.key == key) {
+                    node.val = value;
+                    break;
+                } else if (node.next == null) {
+                    node.next = new Node(key, keyIndex, value);
+                    ++size;
+                    break;
+                }
+                node = node.next;
+            }
+        } else {
+            arrays[keyIndex] = new Node(key, keyIndex, value);
+            ++size;
+        }
+    }
+
+    private int getIndex(int key) {
+        return key & (capacity - 1);
+    }
+
+    /**
+     * 注意:扩容后需要重新计算key值的索引.
+     *
+     * @author tiankai.me@gmail.com on 2023-08-09 13:55.
+     */
+    private void resize(int newSize) {
+        if (newSize > capacity * loadFactor && capacity < Integer.MAX_VALUE) {
+            int newCapacity = (capacity >= (Integer.MAX_VALUE) >> 1) ? Integer.MAX_VALUE : capacity << 1;
+            Object[] newArray = new Object[newCapacity];
+            for (Object array : arrays) {
+                if (array != null) {
+                    Node node = (Node) array;
+                    while (node != null) {
+                        int newKeyIndex = node.key & (newCapacity - 1);
+                        Node newNode = new Node(node.key, newKeyIndex, node.val);
+                        if (newArray[newKeyIndex] != null) newNode.next = (Node) newArray[newKeyIndex];
+                        newArray[newKeyIndex] = newNode;
+                        node = node.next;
+                    }
+                }
+            }
+            arrays = newArray;
+            capacity = newCapacity;
+        }
+    }
+
+    public int get(int key) {
+        final int keyIndex = getIndex(key);
+        if (arrays[keyIndex] != null) {
+            Node node = (Node) arrays[keyIndex];
+            while (node != null) {
+                if (node.key == key) return node.val;
+                node = node.next;
+            }
+        }
+        return -1;
+    }
+
+    public void remove(int key) {
+        int keyIndex = getIndex(key);
+        if (arrays[keyIndex] != null) {
+            Node node = (Node) arrays[keyIndex], prev = null;
+            while (node != null) {
+                if (node.key == key && prev == null) {
+                    arrays[keyIndex] = node.next;
+                    --size;
+                    break;
+                } else if (node.key == key && prev != null) {
+                    prev.next = node.next;
+                    --size;
+                    break;
+                } else prev = node;
+                node = node.next;
+            }
+        }
+    }
+}
+
+class Node {
+    int key;
+    int keyIndex;
+    int val;
+    Node next;
+
+    public Node(int key, int keyIndex, int val) {
+        this.key = key;
+        this.keyIndex = keyIndex;
+        this.val = val;
+    }
+}
+
+/**
+ * 分离链接+红黑树
+ *
+ * @author tiankai.me@gmail.com on 2023-08-09 15:30.
+ */
+class MyHashMapTree {
+    private static class TreeNode {
+        private int key;
+        private int value;
+        private boolean color;
+        private TreeNode left;
+        private TreeNode right;
+        private TreeNode parent;
+
+        private TreeNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private static final boolean RED = false;
+    private static final boolean BLACK = true;
+    private TreeNode[] hashtable = new TreeNode[1024];
+    private int currentSize;
+
+    public void put(int key, int value) {
+        if (currentSize >= hashtable.length) {
+            resize(); // 从结果来看，加载因子选 1.0 效率较高。
+        }
+        int index = key & (hashtable.length - 1);
+        insert(index, new TreeNode(key, value));
+    }
+
+    public int get(int key) {
+        int index = key & (hashtable.length - 1);
+        TreeNode node = getNode(index, key);
+        return node == null ? -1 : node.value;
+    }
+
+    public void remove(int key) {
+        int index = key & (hashtable.length - 1);
+        delete(index, key);
+    }
+
+    // 对哈希表进行扩容。
+    private void resize() {
+        TreeNode[] oldTable = hashtable;
+        hashtable = new TreeNode[hashtable.length << 1];
+        for (TreeNode root : oldTable) {
+            postorderTraversal(root);
+        }
+        currentSize >>= 1;
+    }
+
+    // 获取指定位置上的指定结点。
+    private TreeNode getNode(int index, int key) {
+        TreeNode current = hashtable[index];
+        while (current != null) {
+            if (current.key == key) {
+                break;
+            }
+            if (current.key < key) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        return current;
+    }
+
+    // 在指定位置上插入结点。
+    private void insert(int index, TreeNode insert) {
+        TreeNode current = hashtable[index], parent = null; // 分别保存当前结点及其父结点。
+        while (current != null) {
+            parent = current;
+            if (current.key == insert.key) {
+                current.value = insert.value;
+                return;
+            }
+            if (current.key < insert.key) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        insert.parent = parent;
+        if (parent == null) {
+            hashtable[index] = insert;
+        } else if (parent.key < insert.key) {
+            parent.right = insert;
+        } else {
+            parent.left = insert;
+        }
+        currentSize++;
+        fixAfterInsertion(index, insert);
+    }
+
+    // 删除指定位置上的指定结点。
+    private void delete(int index, int key) {
+        TreeNode delete = getNode(index, key);
+        if (delete == null) {
+            return;
+        }
+        if (delete.left != null && delete.right != null) {
+            TreeNode successor = delete.right;
+            while (successor.left != null) {
+                successor = successor.left;
+            }
+            delete.key = successor.key;
+            delete.value = successor.value;
+            delete = successor;
+        }
+        TreeNode replacement = delete.left == null ? delete.right : delete.left;
+        if (replacement == null) {
+            fixAfterDeletion(index, delete);
+            if (delete.parent == null) {
+                hashtable[index] = null;
+            } else if (delete.parent.left == delete) {
+                delete.parent.left = null;
+            } else {
+                delete.parent.right = null;
+            }
+        } else { // 被删除的结点只有一个子结点，那它一定是黑色结点，且它的子结点为红色。
+            replacement.parent = delete.parent;
+            if (delete.parent == null) {
+                hashtable[index] = replacement;
+            } else if (delete.parent.left == delete) {
+                delete.parent.left = replacement;
+            } else {
+                delete.parent.right = replacement;
+            }
+            replacement.color = BLACK;
+        }
+        currentSize--;
+    }
+
+    // 对插入后的结点进行调整。
+    private void fixAfterInsertion(int index, TreeNode insert) {
+        while (colorOf(insert.parent) == RED) { // 只有父结点是红色才进行处理。
+            // 分别保存当前结点的父结点、叔父结点、祖父结点。
+            TreeNode parent = insert.parent, uncle = sibling(parent), grand = parent.parent;
+            grand.color = RED; // 不管是哪种情况，祖父结点都需要染成红色。
+            if (colorOf(uncle) == BLACK) { // 叔父结点为黑色。
+                if (grand.left == parent) {
+                    if (parent.right == insert) {
+                        rotationLeft(index, parent); // LR 情况：先对父结点进行左旋转。
+                        parent = insert;
+                    }
+                    rotationRight(index, grand); // LL 情况：对祖父结点进行右旋转。
+                } else {
+                    if (parent.left == insert) {
+                        rotationRight(index, parent); // RL 情况：先对父结点进行右旋转。
+                        parent = insert;
+                    }
+                    rotationLeft(index, grand); // RR 情况：对祖父结点进行左旋转。
+                }
+                parent.color = BLACK; // 将旋转后的中心结点染成黑色。
+                insert = hashtable[index]; // 处理完直接退出循环。
+            } else { // 叔父结点为红色，则将父结点与叔父结点都染成黑色，将祖父结点作为新插入的结点继续处理。
+                uncle.color = BLACK;
+                parent.color = BLACK;
+                insert = grand;
+            }
+        }
+        hashtable[index].color = BLACK; // 根结点必须是黑色。
+    }
+
+    // 对删除后的结点进行调整。
+    private void fixAfterDeletion(int index, TreeNode delete) {
+        while (delete.parent != null && delete.color == BLACK) { // 只有删除的是黑色结点才进行处理。
+            // 分别保存当前结点的父结点、兄弟结点。
+            TreeNode parent = delete.parent, sibling = sibling(delete);
+            if (sibling.color == BLACK) { // 兄弟结点是黑色。
+                if (colorOf(sibling.left) == BLACK && colorOf(sibling.right) == BLACK) { // 兄弟结点没有红色子结点。
+                    if (parent.color == BLACK) {
+                        delete = parent;
+                    }
+                    parent.color = BLACK;
+                    sibling.color = RED;
+                } else { // 兄弟结点有红色子结点。
+                    if (parent.left == sibling) {
+                        if (colorOf(sibling.left) == BLACK) {
+                            rotationLeft(index, sibling); // LR 情况：先对兄弟结点进行左旋转。
+                            sibling = sibling.parent;
+                        }
+                        rotationRight(index, parent); // LL 情况：对父结点进行右旋转。
+                    } else {
+                        if (colorOf(sibling.right) == BLACK) {
+                            rotationRight(index, sibling); // RL 情况：先对兄弟结点进行右旋转。
+                            sibling = sibling.parent;
+                        }
+                        rotationLeft(index, parent); // RR 情况：对父结点进行左旋转。
+                    }
+                    sibling.color = parent.color; // 旋转后中心结点继承父结点的颜色。
+                    sibling.left.color = BLACK;
+                    sibling.right.color = BLACK;
+                    delete = hashtable[index]; // 处理完直接退出循环。
+                }
+            } else { // 兄弟结点是红色。
+                if (parent.left == sibling) {
+                    rotationRight(index, parent);
+                } else {
+                    rotationLeft(index, parent);
+                }
+                parent.color = RED;
+                sibling.color = BLACK;
+            }
+        }
+    }
+
+    // 获取指定结点的兄弟结点。
+    private TreeNode sibling(TreeNode node) {
+        if (node.parent.left == node) {
+            return node.parent.right;
+        }
+        return node.parent.left;
+    }
+
+    // 对指定位置上的指定结点进行左旋转。
+    private void rotationLeft(int index, TreeNode node) {
+        TreeNode newRoot = node.right; // 结点的右子结点会成为这颗子树的根结点。
+        node.right = newRoot.left;
+        if (newRoot.left != null) {
+            newRoot.left.parent = node;
+        }
+        newRoot.left = node;
+        newRoot.parent = node.parent;
+        if (node.parent == null) {
+            hashtable[index] = newRoot;
+        } else if (node.parent.left == node) {
+            node.parent.left = newRoot;
+        } else {
+            node.parent.right = newRoot;
+        }
+        node.parent = newRoot;
+    }
+
+    // 对指定位置上的指定结点进行右旋转。
+    private void rotationRight(int index, TreeNode node) {
+        TreeNode newRoot = node.left; // 结点的左子结点会成为这颗子树的根结点。
+        node.left = newRoot.right;
+        if (newRoot.right != null) {
+            newRoot.right.parent = node;
+        }
+        newRoot.right = node;
+        newRoot.parent = node.parent;
+        if (node.parent == null) {
+            hashtable[index] = newRoot;
+        } else if (node.parent.left == node) {
+            node.parent.left = newRoot;
+        } else {
+            node.parent.right = newRoot;
+        }
+        node.parent = newRoot;
+    }
+
+    // 获取指定结点的颜色。
+    private boolean colorOf(TreeNode node) {
+        return node == null || node.color;
+    }
+
+    // 对结点进行后序遍历。
+    private void postorderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postorderTraversal(node.left);
+        postorderTraversal(node.right);
+        node.left = node.right = node.parent = null;
+        node.color = RED;
+        int index = node.key & (hashtable.length - 1);
+        insert(index, node);
+    }
+}
